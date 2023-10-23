ea0737812539fdaaae64962d037982f2ff7be476
Ekaterina Egorova <ekaterinan.egorova@gmail.com>
2023-10-23 01:49:56 -0400

    correct code

--- spinner1.js
+++ spinner1.js
@@ -15,6 +15,7 @@ setTimeout(() => {
   // Need to escape the backslash since it's a special character.
   process.stdout.write("\r\\   ");
 }, 700);
+
 setTimeout(() => {
   process.stdout.write("\r/   ");
 }, 900);
